e35ff1f33419fe53abaf5494e24b51aa9a7eda74
muhabeid <muhabeid@gmail.com>
2022-03-24 01:58:35 +0300

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @author: muhabeid
+ * @s: string to change
+ *
+ * Return: address of s
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+			*(s + i) -= 'a' - 'A';
+		i++;
+	}
+	return (s);
+}
