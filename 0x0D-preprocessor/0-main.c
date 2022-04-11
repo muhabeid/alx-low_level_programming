786f54d2d93b78d2a57ce39633dbcff5c66dccfa
muhabeid <muhabeid@gmail.com>
2022-04-11 23:54:06 +0300

    Test file

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,27 @@
+/**
+ * @file 0-main.c
+ * @author muhabeid
+ * @brief - Test file 
+ * @version 0.1
+ * @date 2022-04-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
+}
