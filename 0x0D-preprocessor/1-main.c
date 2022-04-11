4a5621cb8a865e57190f2609bd26dfc76caa4e9e
muhabeid <muhabeid@gmail.com>
2022-04-12 00:42:41 +0300

    Test file

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,29 @@
+/**
+ * @file 1-main.c
+ * @author muhabeid
+ * @brief Test file for PI 
+ * @version 0.1
+ * @date 2022-04-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    float a;
+    float r;
+
+    r = 98;
+    a = PI * r * r;
+    printf("%.3f\n", a);
+    return (0);
+}
