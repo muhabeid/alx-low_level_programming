f8a96e73bae9b96af4c941b91009614861505093
muhabeid <muhabeid@gmail.com>
2022-04-12 00:57:12 +0300

    3. Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,16 @@
+/**
+ * @file 3-function_like_macro.h
+ * @author muhabeid
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
+
+#endif
