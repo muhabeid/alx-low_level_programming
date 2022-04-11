09854ebe81e85e72171f4e4509da118b309ed7a7
muhabeid <muhabeid@gmail.com>
2022-04-12 01:00:21 +0300

    4. SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,16 @@
+/**
+ * @file 4-sum.h
+ * @author muhabeid
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
+
+#define sum(x, y) ((x) + (y))
+
+#endif
