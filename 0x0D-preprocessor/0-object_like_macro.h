404b61c2e05f249d61e19b2222fdea7918be3e2d
muhabeid <muhabeid@gmail.com>
2022-04-12 00:30:59 +0300

    0. Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,27 @@
+/**
+ * @file 0-object_like_macro.h
+ * @author muhabeid
+ * @brief Object Header file 
+ * @version 0.1
+ * @date 2022-04-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+  // From the general formula of guarding header files 
+  /**
+   * #ifndef _FILE_NAME_H_
+   * #define _FILE_NAME_H_
+   * 
+   * Write code here
+   * 
+   * #endif // #ifndef _FILE_NAME_H
+   */
+
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
