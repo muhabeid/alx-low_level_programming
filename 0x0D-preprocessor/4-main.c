bf68dbf1c03fbe84bacddc89974c3087e932337c
muhabeid <muhabeid@gmail.com>
2022-04-12 01:02:07 +0300

    Test file

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,27 @@
+/**
+ * @file 4-main.c
+ * @author muhabeid
+ * @brief Test File
+ * @version 0.1
+ * @date 2022-04-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int s;
+
+    s = SUM(98, 1024);
+    printf("%d\n", s);
+    return (0);
+}
