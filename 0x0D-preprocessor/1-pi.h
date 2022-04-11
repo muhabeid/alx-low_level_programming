cc56dc9b87b3d18d3c80a89f0e65f974015683dc
muhabeid <muhabeid@gmail.com>
2022-04-12 00:42:22 +0300

    1. Pi

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,16 @@
+/**
+ * @file 1-pi.h
+ * @author muhabeid
+ * @brief 
+ * @version 0.1
+ * @date 2022-04-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef PI
+#define PI
+
+#define PI 3.14159265359
+
+#endif
